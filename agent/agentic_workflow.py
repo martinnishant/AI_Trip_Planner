3894cb91f9ffbe2976793ad6ad5d52e63ba7b1b9
Nishant Tomar <nishanttomar514@gmail.com>
2025-08-23 21:42:29 +0530

    loading the model tools

--- agent/agentic_workflow.py
+++ agent/agentic_workflow.py
@@ -1,6 +1,6 @@
 
 from utils.model_loader import ModelLoader
-from prompt_library.prompt import SYSTEM_PROMPT
+from Prompt_library.prompt import SYSTEM_PROMPT
 from langgraph.graph import StateGraph, MessagesState, END, START
 from langgraph.prebuilt import ToolNode, tools_condition
 from tools.weather_info_tool import WeatherInfoTool
